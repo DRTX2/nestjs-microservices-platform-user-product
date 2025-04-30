77c5c40e19de2f3422bc3f823d4d94704ed7b42d
DRTX2 <davidmanjarres2004@gmail.com>
2025-04-29 22:04:47 -0500

    Update app.module.ts

--- users-service/src/app.module.ts
+++ users-service/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT as string),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true, //	Carga automáticamente todas las entidades que importes con TypeOrmModule.forFeature()
+      synchronize: true, //TRUE solo en desarrollo: crea/modifica tablas según tus entidades; Para produccion usar migraciones
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
