a84a1cfc9bdc460a50ff475d65ef1fe4788c1dba
DRTX2 <davidmanjarres2004@gmail.com>
2025-04-29 22:15:32 -0500

    Add user entity

--- users-service/src/users/entities/user/user.ts
+++ users-service/src/users/entities/user/user.ts
@@ -1,1 +1,32 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string; //UUID
+
+  @Column()
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
+  role: UserRole;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
