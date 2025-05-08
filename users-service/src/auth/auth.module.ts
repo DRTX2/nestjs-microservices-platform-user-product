e84acc64bba4a5962eb9198f9123abdba8ba2e30
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-08 18:16:23 -0500

    Edit auth module

--- users-service/src/auth/auth.module.ts
+++ users-service/src/auth/auth.module.ts
@@ -3,15 +3,22 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports:[UsersModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions:{expiresIn:'1h'}
-    })
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({isGlobal:true}),//ConfigModule allows to load vars from .env file, and forRoot({isGlobal:true}), helps to use that vars globaly
+    JwtModule.registerAsync({// more secure than register, because this method waits for the .env load
+      imports:[ConfigModule], // before use the service we need to charge the module and then we can interact with the service
+      inject:[ConfigService],
+      useFactory:(configService:ConfigService)=>({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
