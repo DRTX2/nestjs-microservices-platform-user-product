3894b997e25559c898d2b371bbe9bc646a1356a6
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-08 21:00:33 -0500

    Add <me> route with jwt guard

--- users-service/src/auth/auth.controller.ts
+++ users-service/src/auth/auth.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { AuthResponseDto } from './dto/auth-response.dto';
 import { LoginDto } from './dto/login.dto';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { JwtAuthGuard } from './guards/jwt_auth.guard';
+import { UsersService } from 'src/users/users.service';
+import { toUserResponse } from 'src/users/mappers/user.mapper';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService:AuthService){}
+    constructor(private readonly authService:AuthService,
+        private readonly usersService:UsersService
+    ){}
 
     @Post("register")
     register(@Body() dto:RegisterDto):Promise<AuthResponseDto>{
@@ -16,5 +22,15 @@ export class AuthController {
     @Post("login")
     login(@Body() dto:LoginDto):Promise<AuthResponseDto>{
         return this.authService.login(dto);
+    }
+
+    @Get("me")
+    @UseGuards(JwtAuthGuard)
+    async getProfile(@CurrentUser() user:{userId:string}){// takes just the userId destructuring the value from CurrentUser
+        const fullUser=await this.usersService.findById(user.userId);
+        if(!fullUser)
+            throw new NotFoundException("User not found");
+        
+        return toUserResponse(fullUser);
     }
 }
--- users-service/src/auth/auth.module.ts
+++ users-service/src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [
@@ -19,6 +20,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
