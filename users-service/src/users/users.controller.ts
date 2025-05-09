589ad12cbb8482187424611f154affbda75a0e6e
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-08 23:18:35 -0500

    Add permissions for routes

--- users-service/src/users/users.controller.ts
+++ users-service/src/users/users.controller.ts
@@ -1,6 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt_auth.guard';
+import { toUserResponse } from './mappers/user.mapper';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UserRole } from './entities/user/user';
 
 @Controller('users')
 export class UsersController {
-    
+  constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    const user = await this.usersService.findById(id);
+    if (!user) throw new NotFoundException('User not found');
+    return toUserResponse(user);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @Get('email/:email')
+  async getUserByEmail(@Param('email') email: string) {
+    const user = await this.usersService.findByEmail(email);
+    if (!user) throw new NotFoundException('User not found');
+    return toUserResponse(user);
+  }
 }
--- users-service/src/users/users.module.ts
+++ users-service/src/users/users.module.ts
@@ -3,6 +3,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user/user';
+import { JwtAuthGuard } from 'src/auth/guards/jwt_auth.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])], // prepare table who creates he Repository<User> and enable it to inject that in the module
